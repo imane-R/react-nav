fc424c438d5172cf351eb9425a00c9173ca94578
imane-R <imanebenamar22@gmail.com>
2024-12-20 12:14:09 +0100

    add navbar

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -2,8 +2,14 @@ import { NavLink } from "react-router";
 
 export function Navbar() {
   return (
-    <>
-      <NavLink></NavLink>
-    </>
+    <nav>
+      <div className="flex justify-between items-center max-w-5xl mx-auto">
+        <NavLink to="/">MyApp</NavLink>
+        <div className="flex space-x-4">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+        </div>
+      </div>
+    </nav>
   );
 }
